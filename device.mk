d88512ff2075d89d52201a59d5b8785d47e118a9
Miraclev1 <127039117+Miracleprjkt@users.noreply.github.com>
2024-02-05 00:30:22 +0700

    change to X657B in device path

--- device.mk
+++ device.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-DEVICE_PATH := device/infinix/hot10
+DEVICE_PATH := device/infinix/X657B
 
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)

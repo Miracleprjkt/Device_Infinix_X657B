24f27431e9f8325eea0e4a5e8f1370f8eea117a2
Miraclev1 <127039117+Miracleprjkt@users.noreply.github.com>
2024-02-08 19:53:52 +0700

    use los base and use los products

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/nad_X657B.mk
+   $(LOCAL_DIR)/lineage_X657B.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_X657B-user \
-    nad_X657B-userdebug \
-    nad_X657B-eng
+    lineage_X657B-user \
+    lineage_X657B-userdebug \
+    lineage_X657B-eng
